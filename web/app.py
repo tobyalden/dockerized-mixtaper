6943f03b547ac8218218413f7389b68b2e467036
Toby Alden <toby.alden@discogsinc.com>
2023-09-12 13:47:02 -0700

    Get everything working in Docker

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'mixtapegarden.sqlite'),
+        DATABASE=os.path.join(app.root_path, 'static', 'mixtapegarden.sqlite'),
         MIXES_FOLDER=os.path.join(app.root_path, 'static', 'mixtapes'),
         MIXTAPE_ART_FOLDER=os.path.join(app.root_path, 'static', 'mixtape_art'),
         USER_AVATAR_FOLDER=os.path.join(app.root_path, 'static', 'user_avatars')
