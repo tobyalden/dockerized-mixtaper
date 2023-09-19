0bf6fb670610b6316068ab2f35935af7d23e98ae
Toby Alden <toby.alden@discogsinc.com>
2023-09-19 11:42:20 -0700

    Add download button to index

--- web/mixtape.py
+++ web/mixtape.py
@@ -30,8 +30,7 @@ job_queue = Queue(connection=redis)
 def index():
     db = get_db()
     mixtapes = db.execute(
-        'SELECT m.id, m.url, m.art, m.title, m.body, m.created, m.author_id, u.username, u.avatar, COUNT(t.mixtape_id) as track_count'
-        # 'SELECT m.id, m.url, m.title, m.body, m.created, m.author_id, u.username'
+        'SELECT m.id, m.url, m.art, m.title, m.body, m.created, m.author_id, m.locked, m.converted, u.username, u.avatar, COUNT(t.mixtape_id) as track_count'
         ' FROM mixtape m'
         ' INNER JOIN user u ON m.author_id = u.id'
         ' LEFT JOIN track t ON m.id = t.mixtape_id'
