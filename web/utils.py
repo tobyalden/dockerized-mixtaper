5ca099948686da084fe861d48c830b2f20761f69
Toby Alden <toby.alden@discogsinc.com>
2025-11-28 21:49:29 -0800

    Fix site

--- web/utils.py
+++ web/utils.py
@@ -68,8 +68,7 @@ def convert_mixtape(youtube_ids, mixtape_url):
         "print_to_file": {
             "post_process": [("%(autonumber)s - %(title)s", tracklist_path)]
         },
-        "username": "oauth2",
-        "password": "",
+        "cookiefile": "./static/cookies.txt",
     }
 
     print("going to download")
